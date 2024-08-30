74981878a4548994c03b09c1f4d2d896293658d6
Muniabi <infotkahn@yandex.ru>
2024-08-30 18:40:34 +0300

    добавил карточку товара

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import {
     TopBar,
 } from "@/components/shared";
 import { Categories } from "@/components/shared";
-import { Button } from "@/components/ui/button";
+import { ProductCard } from "@/components/shared";
+import { Button } from "@/components/ui";
 import { Filter } from "lucide-react";
 
 export default function Home() {
@@ -28,8 +29,17 @@ export default function Home() {
                         <Filters />
                     </div>
                     {/* {Список товаров} */}
-                    <div className="">
-                        TovaryTovaryTovaryTovaryTovaryTovaryTovaryTovaryTovaryTovaryTovaryTovaryTovaryTovary
+                    <div className="flex-1">
+                        <div className="flex flex-col gap-16">
+                            <ProductCard
+                                id={0}
+                                name={"Сырный цыпленок"}
+                                price={543}
+                                imageUrl={
+                                    "https://media.dodostatic.net/image/r:584x584/11EE7D614CBE0530B7234B6D7A6E5F8E.avif"
+                                }
+                            />
+                        </div>
                     </div>
                 </div>
             </Container>
